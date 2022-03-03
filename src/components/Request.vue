eec574aa8f10fe16ebceac1346dcf9e66f2b1567
Simon Thiboutôt <simonthiboutot@hotmail.com>
2022-03-03 06:29:09 -0500

    Display HTTP request and response.

---
+++ src/components/Request.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { HttpAdapter } from 'jsonapi-metal-client'
+
+defineProps<{ request: HttpAdapter.AdapterRequest }>()
+
+</script>
+
+<template>
+  <h3 class="text-lg">Request</h3>
+  <dl>
+    <dt>URL</dt>
+    <dd class="mb-1">
+      <pre class="overflow-y-auto text-xs text-gray-700">{{ request.url }}</pre>
+    </dd>
+
+    <dt>Headers</dt>
+    <dd class="mb-1">
+      <pre class="overflow-y-auto text-xs text-gray-700">{{ request.headers }}</pre>
+    </dd>
+
+    <dt>Body</dt>
+    <dd class="mb-1">
+      <pre class="overflow-y-auto text-xs text-gray-700">{{ request.body }}</pre>
+    </dd>
+  </dl>
+</template>
---
+++ src/components/Response.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { HttpAdapter } from 'jsonapi-metal-client'
+
+const props = defineProps<{ response: HttpAdapter.AdapterResponse }>()
+
+let body = props.response.body
+try {
+  if (body) {
+    body = JSON.stringify(JSON.parse(body), null, 2)
+  }
+} catch (e) {}
+
+</script>
+
+<template>
+  <h3 class="text-lg">Response</h3>
+  <dl>
+    <dt>Status</dt>
+    <dd class="mb-1">
+      <pre class="overflow-y-auto text-xs text-gray-700">{{ response.status }}</pre>
+    </dd>
+
+    <dt>Headers</dt>
+    <dd class="mb-1">
+      <pre class="overflow-y-auto text-xs text-gray-700">{{ response.headers }}</pre>
+    </dd>
+
+    <dt>Body</dt>
+    <dd class="mb-1">
+      <pre class="overflow-y-auto text-xs text-gray-700">{{ body }}</pre>
+    </dd>
+  </dl>
+</template>
